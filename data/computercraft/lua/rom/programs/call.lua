0a67ffeab000b4db85664b459276eee2e3880301
minerbadd <51465461+minerbadd@users.noreply.github.com>
2024-08-07 09:08:58 -0700

    Docs and more

---
+++ data/computercraft/lua/rom/programs/call.lua
@@ -0,0 +1,9 @@
+--[[
+## Remote Call Command (Generalized off-site) 
+```md
+--:? rom/help/call.txt <- **Generalized Call Command Line Interface** -> muse/docs/programs/call.md  
+```Lua
+--]]
+-- TODO: like, e.g. `farmer` but id from qualified name
+
+print("Not implemented")
---
+++ data/computercraft/lua/rom/programs/roll.lua
@@ -0,0 +1,10 @@
+--[[
+## Roll Call Command (Broadcast on "MP" Muse Presence Protocol)
+```md
+--:? rom/help/roll.txt <- **Roll Call Command Line Interface** -> muse/docs/programs/roll.md  
+```Lua
+--]]
+-- TODO: rednet.broadcast id on "MP", received on .roll to call dds.roll: 
+-- dds.roll: rednet.send(id, computerID..": "..label.." on "..site..", "..#places.." places, "MS")
+
+print("Not implemented")
